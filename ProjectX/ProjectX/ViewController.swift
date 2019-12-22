9bc76df7e5a20cf5e5776b4e1eaabcda358ec098
Sameh Mabrouk <mabrouksameh@gmail.com>
2019-12-22 14:31:16 +0100

    Remove unused code

---
+++ ProjectX/ProjectX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectX
+//
+//  Created by Sameh Mabrouk on 22/12/2019.
+//  Copyright © 2019 Sameh Mabrouk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
