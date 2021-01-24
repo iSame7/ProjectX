dff677027de712b14de6bc9e57d48197e7fc85b1
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-01-24 22:54:03 +0100

    Add app config files

---
+++ ProjectX/ProjectX/App/Config/AppConfig+Client.swift
@@ -0,0 +1,14 @@
+//
+//  AppConfig+Client.swift
+//  ProjectX
+//
+//  Created by Sameh Mabrouk on 24/01/2021.
+//  Copyright © 2021 Sameh Mabrouk. All rights reserved.
+//
+
+import Core
+
+extension AppConfig: CoreConfigurable {
+    public static var baseURL = Environment.string(for: .baseURL)
+    public static var grahQLBaseURL = baseURL + "/api/graphql"
+}
