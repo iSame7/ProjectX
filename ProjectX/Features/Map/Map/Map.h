28539722097fe005b428c4d715a0e990d77e40b2
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-01-26 23:55:50 +0100

    Create map module

---
+++ ProjectX/Features/Map/Map/Map.h
@@ -0,0 +1,18 @@
+//
+//  Map.h
+//  Map
+//
+//  Created by Sameh Mabrouk on 26/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Map.
+FOUNDATION_EXPORT double MapVersionNumber;
+
+//! Project version string for Map.
+FOUNDATION_EXPORT const unsigned char MapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Map/PublicHeader.h>
+
+
