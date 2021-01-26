1c21c53480fd5a803d8688ef1c3a63ec700ae1c4
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-01-26 23:52:23 +0100

    Add CocoaLumberjack pod and logging utility

---
+++ ProjectX/Common/Utils/Utils/Logging/Logging.swift
@@ -0,0 +1,94 @@
+//
+//  Logging.swift
+//  Utils
+//
+//  Created by Sameh Mabrouk on 26/01/2021.
+//
+
+import CocoaLumberjack
+import Foundation
+
+public protocol Logging {
+    var loggingCategory: String { get }
+
+    func logDebug(_ message: String, function: StaticString, file: StaticString, line: UInt)
+    func logInfo(_ message: String, function: StaticString, file: StaticString, line: UInt)
+    func logWarning(_ message: String, function: StaticString, file: StaticString, line: UInt)
+    func logError(_ message: String, function: StaticString, file: StaticString, line: UInt)
+}
+
+public extension Logging {
+
+    /// The category with which the class that conforms to the `Logging`-protocol is logging.
+    var loggingCategory: String {
+        return "default"
+    }
+
+    func logDebug(_ message: String, function: StaticString = #function, file: StaticString = #file, line: UInt = #line) {
+        DDLogDebug("🐞 \(message)", file: file, function: function, line: line, tag: loggingCategory)
+    }
+
+    func logInfo(_ message: String, function: StaticString = #function, file: StaticString = #file, line: UInt = #line) {
+        DDLogInfo("📋 \(message)", file: file, function: function, line: line, tag: loggingCategory)
+    }
+
+    func logWarning(_ message: String, function: StaticString = #function, file: StaticString = #file, line: UInt = #line) {
+        DDLogWarn("❗️ \(message)", file: file, function: function, line: line, tag: loggingCategory)
+    }
+
+    func logError(_ message: String, function: StaticString = #function, file: StaticString = #file, line: UInt = #line) {
+        DDLogError("🔥 \(message)", file: file, function: function, line: line, tag: loggingCategory)
+    }
+}
+
+public final class LogHandler: Logging {
+
+    public static var isSetup = false
+
+    /// Can be called multiple times, will only setup once
+    public static func setup() {
+        guard !isSetup else {
+            DDLogDebug("🐞 Logging has already been setup before", file: #file, function: #function, line: #line, tag: "default")
+
+            return
+        }
+
+        isSetup = true
+
+        let level = Bundle.main.infoDictionary?["LOG_LEVEL"] as? String ?? "debug"
+
+        switch level {
+        case "debug":
+            dynamicLogLevel = .debug
+        case "info":
+            dynamicLogLevel = .info
+        case "warn":
+            dynamicLogLevel = .warning
+        case "error":
+            dynamicLogLevel = .error
+        case "none":
+            dynamicLogLevel = .off
+        default:
+            dynamicLogLevel = .off
+        }
+
+        DDLog.add(DDOSLogger.sharedInstance) // Uses os_log
+        let fileLogger: DDFileLogger = DDFileLogger() // File Logger
+        fileLogger.rollingFrequency = 60 * 60 * 24 // 24 hours
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        DDLog.add(fileLogger)
+
+        DDLogDebug("🐞 Logging has been setup", file: #file, function: #function, line: #line, tag: "default")
+    }
+
+    public static func logFiles() -> [URL] {
+        guard let fileLogger = DDLog.allLoggers.filter({ $0 is DDFileLogger }).first as? DDFileLogger else {
+            #if DEBUG
+                assertionFailure("File Logger Not Found")
+            #endif
+            print("File Logger not Found")
+            return []
+        }
+        return fileLogger.logFileManager.sortedLogFilePaths.compactMap { URL(fileURLWithPath: $0) }
+    }
+}
--- ProjectX/Pods/Target Support Files/Pods-Location-LocationTests/Pods-Location-LocationTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Location_LocationTests : NSObject
-@end
-@implementation PodsDummy_Pods_Location_LocationTests
-@end
