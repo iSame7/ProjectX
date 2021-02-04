65acf7efe9a48c65e16bdc01fb616b583898c580
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-02-04 16:31:51 +0100

    Create VenueDetails module

---
+++ ProjectX/Features/VenueDetails/VenueDetails/VenueDetails.h
@@ -0,0 +1,18 @@
+//
+//  VenueDetails.h
+//  VenueDetails
+//
+//  Created by Sameh Mabrouk on 04/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VenueDetails.
+FOUNDATION_EXPORT double VenueDetailsVersionNumber;
+
+//! Project version string for VenueDetails.
+FOUNDATION_EXPORT const unsigned char VenueDetailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VenueDetails/PublicHeader.h>
+
+
