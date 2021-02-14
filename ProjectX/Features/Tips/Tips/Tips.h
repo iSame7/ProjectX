be8f7b8b3d32678b4f88cfc51e7d9ac40de0bd65
Sameh Mabrouk <mabrouksameh@gmail.com>
2021-02-14 14:11:41 +0100

    Create Tips module

---
+++ ProjectX/Features/Tips/Tips/Tips.h
@@ -0,0 +1,18 @@
+//
+//  Tips.h
+//  Tips
+//
+//  Created by Sameh Mabrouk on 14/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tips.
+FOUNDATION_EXPORT double TipsVersionNumber;
+
+//! Project version string for Tips.
+FOUNDATION_EXPORT const unsigned char TipsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tips/PublicHeader.h>
+
+
